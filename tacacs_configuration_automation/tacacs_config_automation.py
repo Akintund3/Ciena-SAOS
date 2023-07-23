f949dace7ac11baf207f2a452a9d7a87e5068e67
Akintunde Adeniran <131524525+Akintund3@users.noreply.github.com>
2023-07-23 03:57:10 +0100

    Update tacacs_config_automation.py

--- tacacs_configuration_automation/tacacs_config_automation.py
+++ tacacs_configuration_automation/tacacs_config_automation.py
@@ -1,95 +1,74 @@
-#!/usr/bin/env python
-
-
 from getpass import getpass
-from operator import ne
-from netmiko import ConnectHandler, ConnectionException, NetMikoAuthenticationException, NetMikoTimeoutException
+from netmiko import ConnectHandler, NetMikoAuthenticationException, NetMikoTimeoutException
 
-#Enter the login (default) credentials
-username = input('Enter Username: ')
-password = getpass()
+def connect_to_device(device_type, host, username, password):
+    try:
+        net_ssh = ConnectHandler(
+            device_type=device_type,
+            host=host,
+            username=username,
+            password=password,
+        )
+        return net_ssh
+    except (NetMikoAuthenticationException, NetMikoTimeoutException) as e:
+        print(f"Failed to connect to {host}: {e}")
+        return None
 
-#unpacking each of the input files
-with open('tacacs_server.txt', 'r') as file:              
-    t_ping = file.readlines()
+def check_tacacs_servers(net_ssh, servers):
+    tacacs_sh = net_ssh.send_command('tacacs show')
+    return all(server in tacacs_sh for server in servers)
 
-with open('network_elements_mgmt_ip.txt', 'r') as file:
-    cet = file.readlines()
+if __name__ == "__main__":
+    username = input('Enter Username: ')
+    password = getpass()
 
-with open('general_tacacs_config.txt', 'r') as file:
-    t_gen = file.readlines()
+    with open('tacacs_ping.txt', 'r') as file:
+        t_ping = file.read()
 
-with open('saos6_tacacs_config.txt', 'r') as file:
-    s6 = file.readlines()
+    with open('tacacs_cet.txt', 'r') as file:
+        cet = file.readlines()
 
-with open('saos8_tacacs_config.txt', 'r') as file:
-    s8 = file.readlines()
+    with open('tacacs_config_gen.txt', 'r') as file:
+        t_gen = file.read()
 
+    with open('tacacs_saos6.txt', 'r') as file:
+        s6 = file.read()
 
+    with open('tacacs_saos8.txt', 'r') as file:
+        s8 = file.read()
 
-#Creating dictionary representing the devices in cet
-for i in cet: 
-    
-    for k in range(len(cet)):
-        
+    tacacs_servers = ['zz.zz.zz.zz', 'xx.xx.xx.xx', 'yy.yy.yy.yy']
+
+    for i in cet:
+        host = i.strip()
         metro_cet = {
             'device_type': 'ciena_saos',
-            'host': i,
+            'host': host,
             'username': username,
             'password': password,
-            }
-        
-        
-        
+        }
 
-        try:
-            #Establish an SSH connection by passing in the device dictionary  
-            net_ssh = ConnectHandler(**metro_cet) 
-            #Pinging for reachability to TACACS
-            out_ping = net_ssh.send_config_set(t_ping)            
-            print()
-            print('-'*75)
-            print(f'Connectinng to device {i}')
-            print()
-            print('-'*75)
-            print()
+        net_ssh = connect_to_device(**metro_cet)
+        if net_ssh:
+            print(f"\n{'-'*75}\nConnecting to device {host}\n{'-'*75}\n")
+            out_ping = net_ssh.send_config_set(t_ping)
 
-            #ping success on at least one tacacs server
-            if '3 packets transmitted, 3 packets received, 0% packet loss' in out_ping :           
-
-                #generic tacacs config for saos6 and saos8                
+            if '3 packets transmitted, 3 packets received, 0% packet loss' in out_ping:
                 net_ssh.send_config_set(t_gen)
-                tacacs_sh = net_ssh.send_command('tacacs show')
 
-                if 'zz.zz.zz.zz' and 'xx.xx.xx.xx' and 'yy.yy.yy.yy' in tacacs_sh:
-
+                if check_tacacs_servers(net_ssh, tacacs_servers):
                     saos_v = net_ssh.send_command('software show')
-                    
-                    #tacacs config for saos8
                     if 'rel_saos5170_8' in saos_v:
                         net_ssh.send_config_set(s8)
-                        tacacs_sh = net_ssh.send_command('tacacs show')
-                        print(tacacs_sh)
-                        
-                    #tacacs config for saos6
-                    else :
-                        net_ssh.send_command('tacacs set preferred-source-ip ' + i)
+                    else:
+                        net_ssh.send_command(f'tacacs set preferred-source-ip {host}')
                         net_ssh.send_config_set(s6)
-                        tacacs_sh = net_ssh.send_command('tacacs show')
-                        print(tacacs_sh)
-                        
-                #At leasest one of the tacacs server is reachable
-                else :
-                    print(f'A least one of the three tacacs severs not configured on {i}')
-                    
-            #All the tacacs server are unreachable
-            else :
 
-                print(f'{i} can not reach at least one of the three tacacs severs')
-                
-                
-        #connection failure or timed out       
-        except ConnectionError or NetMikoTimeoutException or NetMikoAuthenticationException or ConnectionException :
-            print('Ne Timed out or authentication failure or connection error')
-
-        
+                    tacacs_sh = net_ssh.send_command('tacacs show')
+                    print(tacacs_sh)
+                else:
+                    print(f"At least one of the three tacacs servers is not configured on {host}")
+            else:
+                print(f"{host} cannot reach at least one of the three tacacs servers")
+        else:
+            print(f"Failed to connect to {host}")
